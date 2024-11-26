9b3803c7b3f41da1ecd09af7a28c807452e543cf
Binoculars7 <elitespot.com@gmail.com>
2024-11-26 15:25:04 +0100

    v19

---
+++ php project/firstproject.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP First Project</title>
+</head>
+<body>
+
+
+
+    
+    <?php 
+    echo '<h1>New Website</h1>';
+    echo "<div style='background:red; color:white;'>New Project</div>";
+
+    $x = 10;
+    
+    $y = 4;
+
+    $ans = $x + $y;
+
+    echo $ans;
+
+
+    ?>
+
+</body>
+</html>
